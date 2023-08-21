54f193704ebc9d7eda33648de31e21c6d2973f55
zipkero <zipkero@gmail.com>
2023-08-21 23:15:51 +0900

    added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,8 @@ const styles = css`
 function App() {
   return (
     <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+
       <Global styles={styles} />
       <TodoContainer>
         <TodoHeader />
