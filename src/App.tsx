8ffc26d610f976e6b3d389d7626c1902511df36c
zipkero <zipkero@gmail.com>
2023-08-22 13:18:49 +0900

    remove emotion and apply base tailwind style

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
-import { css, Global } from "@emotion/react";
 import TodoContainer from "./components/TodoContainer.tsx";
 import TodoHeader from "./components/TodoHeader.tsx";
 import TodoForm from "./components/TodoForm.tsx";
 import TodoList from "./components/TodoList.tsx";
 
-const styles = css`
-  body {
-    background: #e9ecef;
-  }
-`;
-
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-
-      <Global styles={styles} />
       <TodoContainer>
         <TodoHeader />
         <TodoList />
--- src/components/TodoContainer.tsx
+++ src/components/TodoContainer.tsx
@@ -1,24 +1,14 @@
-import styled from "@emotion/styled";
 import { ReactNode } from "react";
-
-const Container = styled.div`
-  width: 512px;
-  height: 768px;
-
-  position: relative;
-  background: white;
-  border-radius: 16px;
-  box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.04);
-
-  margin: 96px auto 32px;
-  display: flex;
-  flex-direction: column;
-`;
 
 interface Props {
   children: ReactNode;
 }
 
 export default function TodoContainer({ children }: Props) {
-  return <Container>{children}</Container>;
+  //return <Container>{children}</Container>;
+  return (
+    <div className="max-w-xl min-h-full relative bg-white mx-auto flex flex-col rounded-2xl shadow-lg">
+      {children}
+    </div>
+  );
 }
