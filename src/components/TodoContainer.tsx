fe672d60df014de1a6bdc50268562ea9acf636ac
zipkero <zipkero@gmail.com>
2023-08-22 16:04:34 +0900

    removed header emotion style and added header tailwind style

--- src/components/TodoContainer.tsx
+++ src/components/TodoContainer.tsx
@@ -5,10 +5,9 @@ interface Props {
 }
 
 export default function TodoContainer({ children }: Props) {
-  //return <Container>{children}</Container>;
   return (
-    <div className="max-w-xl min-h-full relative bg-white mx-auto flex flex-col rounded-2xl shadow-lg">
+    <main className="max-w-xl min-h-full relative bg-white mx-auto flex flex-col rounded-2xl shadow-lg">
       {children}
-    </div>
+    </main>
   );
 }
